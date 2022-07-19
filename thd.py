2fe6675963eb7a29546eefa5dfc5e21825d5550e
rvonhoog <rvonhoog@pm.me>
2022-07-19 12:35:47 -0700

    ✨ Feat: Added postLog section

--- thd.py
+++ thd.py
@@ -61,11 +61,57 @@ def postLog():
     choice = input("Select 1,2, or 3: ")
 
     if choice == "1":
-        print("1")
+        print("'Luxer One Retail Support this is Alex'")
+        print("You tell him how you are looking for your package, traced it to THD, and found his name in the maintence log")
+        print("Alex lets out an audible groan")
+        print("'Yeah they are always adding me to these things.'")
+        print("'I didn't even need to be there'")
+        print("'Andy and I just stood around while installs did their thing'")
+        inventory.append("sus")
+        print(inventory)
+
     elif choice == "2":
-        print("2")
+        print("'Hello this is Andrew.'")
+        print("You tell how you are looking for your package, traced it to THD, and found his name in the maintence log")
+        print("'Oh yes of course I was there. Really not an install that can be completed without my assitance.'")
+        if "sus" in inventory:
+            print ("You tell him how you just got off the phone with Alex and he told you a different story.")
+            print ("Andy quickly hangs up on you.")
+            print ("You and the party share a concerned look")
+            rand = random.randrange(5,17)
+        else:
+            print ("You call him out about helping with the install.")
+            print ("'The gull to say I was not needed for it to be completed. How dare you!'")
+            rand = random.randrange(1,17)
+        if rand >= 8:
+            print ("The MOD arrives and lets you know they have a video that might be of some use.")
+            if "vikki" in party:
+                print("While watching the video Vikki lets you know that is reconizes the store shown and can take you to it.")
+                print("After arriving at the location you notice Andy's car and follow him back to his hideout.")
+                inventory.append("lockerLocation")
+            else:
+                print("You watch the video and it appears to be a different store that you are not familar with")
+                print("You decide to stake out the next install and follow Andy's car back to the hide out.")
+                inventory.append("lockerLocation")
+
     elif choice == "3":
-        print("3")
+        print("'Hello Luxer One Installs'")
+        print("You tell him how you are looking for your package, traced it to THD, and found his name in the maintence log")
+        print("'Ah yes, I remember that install.'")
+        print("'Went off without a hitch.'")
+        print("'Only strange thing was an Anywhere locker that went missing that day. Nothing about that install though.'")
+        if "lisa" in party:
+            rand = random(1,17)
+            if rand <= 8:
+                print("Lisa is able to help you decipher the rep's description of the location where they dropped off the locker.")
+                inventory.append("lockerLocation")
+            else:
+                party.remove("lisa")
+        else:
+            print("Unable to decipher Install's description of the location you turn to chatbot.")
+            print("Chatbot initally responds rather quickly but doesn't understand query.")
+            print("After some back and forth they are able to assist and you have the location you seek.")
+            inventory.append("lockerLocation")
     else:
         print ("Invalid input please choose 1,2,3, or 4")
 
